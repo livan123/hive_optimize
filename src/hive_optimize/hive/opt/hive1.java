ca3d5db8428ca1b47b5a12ea204be00d9df1690a
livan123 <200877501230.happy@163.com>
2018-07-14 00:46:36 +0800

    Initial commit

---
+++ src/hive_optimize/hive/opt/hive1.java
@@ -0,0 +1,11 @@
+package hive_optimize.hive.opt;
+
+public class hive1 {
+	//创建一张表到hdfs中；
+	
+
+}
+
+
+
+
---
+++ src/hive_optimize/hive/opt/opt1.java
@@ -0,0 +1,10 @@
+package hive_optimize.hive.opt;
+
+public class opt1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
